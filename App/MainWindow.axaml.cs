aada56356fd53fb70bd7e857c25125acd3ebceb9
Emil Jørgensen <emiljorgensen05@gmail.com>
2025-02-19 22:20:20 +0100

    Started work on application.
    Changed theme to "Light".
    Made a mockup of the gui. Main focus was getting the views to change at the click of a button, which now works. YAY.
    Added HomeView and SettingsView axaml files, ready for some great gui work. Easy to add more.
    Work is commented.

--- App/MainWindow.axaml.cs
+++ App/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace App;
 
@@ -7,5 +10,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void HomeButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+	    SetActiveView(HomeViewElement);
+    }
+
+    private void SettingsButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+	    SetActiveView(SettingsViewElement);
+    }
+
+    /// <summary>
+    /// Deactivates all views and activates the view passed as parameter
+    /// </summary>
+    /// <param name="view"> The view that will get activated </param>
+    private void SetActiveView(UserControl view)
+    {
+	    // set IsVisible of all views/userControls with the class "ViewElement" to false
+	    foreach (var child in this.GetVisualDescendants())
+	    {
+		    if (child is UserControl userControl && userControl.Classes.Contains("ViewElement"))
+		    {
+			    userControl.IsVisible = false;
+		    }
+	    }
+	    
+	    // set IsVisible of the view to true
+	    view.IsVisible = true;
     }
 }
