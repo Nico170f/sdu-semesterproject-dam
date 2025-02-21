8aacd012b9f50e983ca2cf1694dc133f9eaf823b
Emil Jørgensen <emiljorgensen05@gmail.com>
2025-02-21 13:24:59 +0100

    Add AddView and update HomeView layout

---
+++ App/Views/AddView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace App.Views;
+
+public partial class AddView : UserControl
+{
+	public AddView ()
+	{
+		InitializeComponent();
+	}
+}
